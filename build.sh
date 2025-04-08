71b5de10a68a92a2f601ff70c36dea1670d42cd1
Jose Contreras (Potter) <jose.contreras.godoy@gmail.com>
2025-04-08 11:09:44 -0600

    Small updates in gitignore and build

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ if [ ! -d build ];then
     mkdir build
 fi
 
-cmake -C initial_cache.cmake -B build &&
+cmake -C initial_cache.cmake -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 &&
 cmake --build build/
 
 cp build/main.exe ./
